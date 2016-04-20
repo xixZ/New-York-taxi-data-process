2d28cf1a843a14bc21ab3163694a348880b54679
Xi <xiz13@uci.edu>
2016-04-20 09:18:59 -0700

    handle multipolygon

--- geoJson2adm/Json2ADMNeighbor.py
+++ geoJson2adm/Json2ADMNeighbor.py
@@ -9,18 +9,22 @@ json_data.close()
 features = j['features']
 
 for feature in features:
-    new_record = '{'
-    new_record += '"id": ' + str(feature['id']) + ', '
-    new_record += '"neighbor_name": "' + feature['properties']['NTAName'] + '",'
-    new_record += '"boro_code": ' + str(feature['properties']['BoroCode']) + ', '
-    coors = feature['geometry']['coordinates'][0]
-    if isinstance(coors[0][0], list):
-        coors = coors[0]
-    new_record += '"spatial_cell": polygon("'
-    polygon = ""
+    coors = feature['geometry']['coordinates']
+    count = 0;
     for coor in coors:
-        polygon = str(coor[1]) + ',' + str(coor[0]) + ' ' + polygon
-    new_record += polygon + '")}\n'
-    fo.write(new_record)
+        count += 1
+        new_record = '{'
+        new_record += '"major_id": ' + str(feature['id']) + ', '
+        new_record += '"minor_id": ' + str(count) + ', '
+        new_record += '"neighbor_name": "' + feature['properties']['NTAName'] + '",'
+        new_record += '"boro_code": ' + str(feature['properties']['BoroCode']) + ', '
+        new_record += '"spatial_cell": polygon("'
+        polygon = ""
+        if isinstance(coor[0][0], list):
+            coor = coor[0]
+        for c in coor:
+            polygon = str(c[1]) + ',' + str(c[0]) + ' ' + polygon
+        new_record += polygon + '")}\n'
+        fo.write(new_record)
 
 fo.close()
